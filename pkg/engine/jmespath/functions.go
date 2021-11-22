0a298585e454a29fb0729f3c622ad1903b2d57a1
Marcel Mueller <marcel.mueller1@rwth-aachen.de>
2021-11-22 19:02:54 +0100

    revert change

--- pkg/engine/jmespath/functions.go
+++ pkg/engine/jmespath/functions.go
@@ -570,7 +570,7 @@ func jpYamlToJson(arguments []interface{}) (interface{}, error) {
 		return nil, err
 	}
 
-	return json, nil
+	return string(json), nil
 }
 
 // InterfaceToString casts an interface to a string type
